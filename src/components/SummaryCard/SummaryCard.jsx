7ad4326d06e8826e620a42cbae86d147574cd97d
Limuel Camangon <limuelmartincamangon@gmail.com>
2025-10-06 19:17:15 +0800

    update

--- src/components/SummaryCard/SummaryCard.jsx
+++ src/components/SummaryCard/SummaryCard.jsx
@@ -1,18 +1,18 @@
 import './summary-card.css';
 
-function SummaryCard() {
+function SummaryCard({ lowTides, highTides }) {
 
     return (
         <>
             <div className="flex flex-row justify-between p-5 gap-5 mt-20 mx-auto rounded-3xl bg-[#E8CACA] w-[95%] h-35 custom-inset-shadow">
                 <div className="flex flex-col gap-7 p-3 rounded-xl bg-black/50 w-95 h-full custom-drop-shadow relative">
                     <p className="text-white text-xs md:text-sm font-semibold">Total High Tides</p>
-                    <p className="text-white text-2xl font-bold absolute bottom-2 right-3">100</p>
+                    <p className="text-white text-2xl font-bold absolute bottom-2 right-3">{highTides}</p>
                 </div>
 
                 <div className="flex flex-col gap-7 p-3 rounded-xl bg-black/50 w-95 h-full custom-drop-shadow relative">
                     <p className="text-white text-xs md:text-sm font-semibold">Total Low Tides</p>
-                    <p className="text-white text-2xl font-bold absolute bottom-2 right-3">120</p>
+                    <p className="text-white text-2xl font-bold absolute bottom-2 right-3">{lowTides}</p>
                 </div>
 
                 <div className="flex flex-col gap-7 p-3 rounded-xl bg-black/50 w-95 h-full custom-drop-shadow relative">
