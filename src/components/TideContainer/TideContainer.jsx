4af23c9c5f9e342e1f419802ef32a6cdf721a478
Limuel Camangon <limuelmartincamangon@gmail.com>
2025-10-07 23:13:40 +0800

    update

--- src/components/TideContainer/TideContainer.jsx
+++ src/components/TideContainer/TideContainer.jsx
@@ -9,6 +9,20 @@ function TideContainer() {
     const today = new Date().getDate();
     const todayIndex = tides.findIndex(tide => tide.date == today);
     const tideRefs = useRef([]);
+    const monthButtonData = [
+        { month: "Jan", monthValue: 0 },
+        { month: "Feb", monthValue: 1 },
+        { month: "Mar", monthValue: 2 },
+        { month: "Apr", monthValue: 3 },
+        { month: "May", monthValue: 4 },
+        { month: "Jun", monthValue: 5 },
+        { month: "Jul", monthValue: 6 },
+        { month: "Aug", monthValue: 7 },
+        { month: "Sep", monthValue: 8 },
+        { month: "Oct", monthValue: 9 },
+        { month: "Nov", monthValue: 10 },
+        { month: "Dec", monthValue: 11 },
+    ];
 
     useEffect(() => {
         fetch("https://bahagonoyapi.web.app/hagonoyTides.json")
@@ -33,36 +47,30 @@ function TideContainer() {
 
     return (
         <>
-            <div className="flex flex-wrap flex-row justify-center items-center w-full gap-1">
-                <button className={`text-white font-semibold ${dateIndex == 0 ? `bg-[#0E2DA6]/30 border-white` : `bg-white/30 border-transparent`} border  p-0.5 rounded-md transition duration-500`} onClick={() => setDateIndex(0)}>Jan</button>
-                <button className={`text-white font-semibold ${dateIndex == 1 ? `bg-[#0E2DA6]/30 border-white` : `bg-white/30 border-transparent`} border  p-0.5 rounded-md transition duration-500`} onClick={() => setDateIndex(1)}>Feb</button>
-                <button className={`text-white font-semibold ${dateIndex == 2 ? `bg-[#0E2DA6]/30 border-white` : `bg-white/30 border-transparent`} border  p-0.5 rounded-md transition duration-500`} onClick={() => setDateIndex(2)}>Mar</button>
-                <button className={`text-white font-semibold ${dateIndex == 3 ? `bg-[#0E2DA6]/30 border-white` : `bg-white/30 border-transparent`} border  p-0.5 rounded-md transition duration-500`} onClick={() => setDateIndex(3)}>Apr</button>
-                <button className={`text-white font-semibold ${dateIndex == 4 ? `bg-[#0E2DA6]/30 border-white` : `bg-white/30 border-transparent`} border  p-0.5 rounded-md transition duration-500`} onClick={() => setDateIndex(4)}>May</button>
-                <button className={`text-white font-semibold ${dateIndex == 5 ? `bg-[#0E2DA6]/30 border-white` : `bg-white/30 border-transparent`} border  p-0.5 rounded-md transition duration-500`} onClick={() => setDateIndex(5)}>Jun</button>
-                <button className={`text-white font-semibold ${dateIndex == 6 ? `bg-[#0E2DA6]/30 border-white` : `bg-white/30 border-transparent`} border  p-0.5 rounded-md transition duration-500`} onClick={() => setDateIndex(6)}>Jul</button>
-                <button className={`text-white font-semibold ${dateIndex == 7 ? `bg-[#0E2DA6]/30 border-white` : `bg-white/30 border-transparent`} border  p-0.5 rounded-md transition duration-500`} onClick={() => setDateIndex(7)}>Aug</button>
-                <button className={`text-white font-semibold ${dateIndex == 8 ? `bg-[#0E2DA6]/30 border-white` : `bg-white/30 border-transparent`} border  p-0.5 rounded-md transition duration-500`} onClick={() => setDateIndex(8)}>Sep</button>
-                <button className={`text-white font-semibold ${dateIndex == 9 ? `bg-[#0E2DA6]/30 border-white` : `bg-white/30 border-transparent`} border  p-0.5 rounded-md transition duration-500`} onClick={() => setDateIndex(9)}>Oct</button>
-                <button className={`text-white font-semibold ${dateIndex == 10 ? `bg-[#0E2DA6]/30 border-white` : `bg-white/30 border-transparent`} border  p-0.5 rounded-md transition duration-500`} onClick={() => setDateIndex(10)}>Nov</button>
-                <button className={`text-white font-semibold ${dateIndex == 11 ? `bg-[#0E2DA6]/30 border-white` : `bg-white/30 border-transparent`} border  p-0.5 rounded-md transition duration-500`} onClick={() => setDateIndex(11)}>Dec</button>
+
+            <div className="flex flex-row flex-wrap items-center justify-center w-full gap-1">
+                {
+                    monthButtonData.map(
+                        (monthData, key) => <button key={key} className={`text-white font-semibold ${dateIndex == monthData.monthValue ? `bg-[#0E2DA6]/30 border-white` : `bg-white/30 border-transparent`} border  p-0.5 rounded-md transition duration-500`} onClick={() => setDateIndex(monthData.monthValue)}>{monthData.month}</button>
+                    )
+                }
             </div>
 
             <div className="flex flex-col gap-5 sm:h-60 md:h-1/4">
-                <div className="flex justify-between px-5 items-center w-full">
-                    <h1 className='text-white font-semibold text-xl'>
+                <div className="flex items-center justify-between w-full px-5">
+                    <h1 className='text-xl font-semibold text-white'>
                         {
                             data.monthlyTide &&
                             data.monthlyTide[dateIndex] &&
                             data.monthlyTide[dateIndex].month || 'Loading...'
                         }
                     </h1>
-                    <h1 className='text-white font-semibold text-xl'>{data.year}</h1>
+                    <h1 className='text-xl font-semibold text-white'>{data.year}</h1>
                 </div>
 
 
-                <div className="flex flex-row gap-5 px-5 w-full h-full overflow-auto overflow-y-hidden">
-                    <div className="flex flex-row gap-5 w-full">
+                <div className="flex flex-row w-full h-full gap-5 px-5 overflow-auto overflow-y-hidden">
+                    <div className="flex flex-row w-full gap-5">
                         {
                             tides.map((tide, key) =>
                                 <div key={key} ref={el => tideRefs.current[key] = el} className='min-w-fit min-h-fit'>
