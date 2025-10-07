5b7ebe568618e103ad7dd7a0566e3c713faf659f
Limuel Camangon <limuelmartincamangon@gmail.com>
2025-10-07 23:27:08 +0800

    updated dynamic button rendering

--- src/components/SummaryCard/SummaryCard.jsx
+++ src/components/SummaryCard/SummaryCard.jsx
@@ -5,19 +5,19 @@ function SummaryCard({ lowTides, highTides }) {
     return (
         <>
             <div className="flex flex-row justify-between p-5 gap-5 mt-20 mx-auto rounded-3xl bg-[#E8CACA] w-[95%] h-35 custom-inset-shadow">
-                <div className="flex flex-col gap-7 p-3 rounded-xl bg-black/50 w-95 h-full custom-drop-shadow relative">
-                    <p className="text-white text-xs md:text-sm font-semibold">Total High Tides</p>
-                    <p className="text-white text-2xl font-bold absolute bottom-2 right-3">{highTides}</p>
+                <div className="relative flex flex-col h-full p-3 gap-7 rounded-xl bg-black/50 w-95 custom-drop-shadow">
+                    <p className="text-xs font-semibold text-white md:text-sm">Total High Tides</p>
+                    <p className="absolute font-bold text-white md:text-2xl sm:text-xl bottom-2 right-3">{highTides}</p>
                 </div>
 
-                <div className="flex flex-col gap-7 p-3 rounded-xl bg-black/50 w-95 h-full custom-drop-shadow relative">
-                    <p className="text-white text-xs md:text-sm font-semibold">Total Low Tides</p>
-                    <p className="text-white text-2xl font-bold absolute bottom-2 right-3">{lowTides}</p>
+                <div className="relative flex flex-col h-full p-3 gap-7 rounded-xl bg-black/50 w-95 custom-drop-shadow">
+                    <p className="text-xs font-semibold text-white md:text-sm">Total Low Tides</p>
+                    <p className="absolute text-2xl font-bold text-white bottom-2 right-3">{lowTides}</p>
                 </div>
 
-                <div className="flex flex-col gap-7 p-3 rounded-xl bg-black/50 w-95 h-full custom-drop-shadow relative">
-                    <p className="text-white text-xs md:text-sm font-semibold">Highest Tide</p>
-                    <p className="text-white text-2xl font-bold absolute bottom-2 right-3">5.2</p>
+                <div className="relative flex flex-col h-full p-3 gap-7 rounded-xl bg-black/50 w-95 custom-drop-shadow">
+                    <p className="text-xs font-semibold text-white md:text-sm">Highest Tide</p>
+                    <p className="absolute font-bold text-white md:text-2xl sm:text-xl bottom-2 right-3">5.2</p>
                 </div>
             </div>
         </>
