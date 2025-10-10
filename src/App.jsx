9e8e11cc9086cdc9a66cc5748de693f94ca61411
Limuel Camangon <limuelmartincamangon@gmail.com>
2025-10-10 14:24:56 +0800

    Added routing and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,17 @@
-import { useEffect, useState } from 'react';
-import Navbar from './components/Navbar/Navbar';
-import SummaryCard from './components/SummaryCard/SummaryCard';
-import TideContainer from './components/TideContainer/TideContainer';
-import './index.css'
+import "./index.css";
+import Home from "./pages/Home/Home";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
 function App() {
-
-  const [monthlyTides, setMonthlyTides] = useState([]);
-  const [totalHighTides, setTotalHighTides] = useState(0);
-  const [totalLowTides, setTotalLowTides] = useState(0);
-
-  useEffect(() => {
-    fetch("https://bahagonoyapi.web.app/hagonoyTides.json")
-      .then((res) => res.json())
-      .then((data) => {
-
-        const freshData = data.monthlyTide;
-
-        console.log(data.monthlyTide);
-        setMonthlyTides(data.monthlyTide);
-
-
-        let highCount = 0;
-        let lowCount = 0;
-
-        freshData.forEach(mt => {
-          mt.dailyTide.forEach(dt => {
-            dt.tide.forEach(t => {
-              if (t.tideLevel >= 3.0) {
-                highCount++;
-              }
-              else {
-                lowCount++;
-              }
-            })
-          })
-        });
-
-        setTotalHighTides(highCount);
-        setTotalLowTides(lowCount);
-
-      })
-
-  }, []);
-
-
-
   return (
     <>
-      <div className="flex flex-col gap-5 bg-[#060F28] w-screen h-screen">
-        <Navbar />
-
-        <section className="flex flex-col w-full min-h-screen gap-5 xl:px-70">
-          <SummaryCard lowTides={totalLowTides} highTides={totalHighTides} />
-
-          <TideContainer />
-        </section>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={""} />
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import SummaryCard from "../../components/SummaryCard/SummaryCard";
+import TideContainer from "../../components/TideContainer/TideContainer";
+import "../../index.css";
+
+function Home() {
+  const [monthlyTides, setMonthlyTides] = useState([]);
+  const [totalHighTides, setTotalHighTides] = useState(0);
+  const [totalLowTides, setTotalLowTides] = useState(0);
+
+  useEffect(() => {
+    fetch("https://bahagonoyapi.web.app/hagonoyTides.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const freshData = data.monthlyTide;
+
+        console.log(data.monthlyTide);
+        setMonthlyTides(data.monthlyTide);
+
+        let highCount = 0;
+        let lowCount = 0;
+
+        freshData.forEach((mt) => {
+          mt.dailyTide.forEach((dt) => {
+            dt.tide.forEach((t) => {
+              if (t.tideLevel >= 3.0) {
+                highCount++;
+              } else {
+                lowCount++;
+              }
+            });
+          });
+        });
+
+        setTotalHighTides(highCount);
+        setTotalLowTides(lowCount);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="flex flex-col gap-5 bg-[#060F28] w-full min-h-full">
+        <Navbar />
+        <section className="flex flex-col w-full min-h-screen gap-5 xl:px-70">
+          <SummaryCard lowTides={totalLowTides} highTides={totalHighTides} />
+
+          <TideContainer />
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default Home;
