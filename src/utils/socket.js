bc91ec618169ac717aa86500899f253df281054f
Limuel <limuelmartincamangon@gmail.com>
2025-12-25 19:38:20 +0800

    added auto reconnection for websockets

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,4 +3,7 @@ import { io } from "socket.io-client";
 export const socket = io("https://hagonoytides-backend-1.onrender.com", {
   autoConnect: false,
   withCredentials: true,
+  reconnection: true,
+  reconnectionAttempts: Infinity,
+  reconnectionDelay: 1000,
 });
