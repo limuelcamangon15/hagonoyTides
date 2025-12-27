bce5246b31ca14c4828da57caeb2d53efff6fcdb
Limuel <limuelmartincamangon@gmail.com>
2025-12-27 15:52:38 +0800

    fixed dynamic rendering

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -16,17 +16,20 @@ import "./weather.css";
 import AuroraBackground from "../ui/AuroraBackground";
 
 function Weather() {
+  const [fetchingData, setFetchingData] = useState(true);
+
   const [weather, setWeather] = useState();
   const [location, setLocation] = useState();
   const [temperature, setTemperature] = useState();
   const [wind, setWind] = useState();
+
   const [time, setTime] = useState(new Date());
 
   const weatherBasedColors = {
-    sunny: ["#FF3C00", "#FFD700", "#00FFFF"],
-    cloudy: ["#6B5B95", "#FF6F61", "#88B04B"],
-    rainy: ["#001F3F", "#39CCCC", "#FF4136"],
-    thunderstorm: ["#FF00FF", "#00FF00", "#FFFF00"],
+    sunny: ["#FFD700", "#FF4500", "#00BFFF"],
+    cloudy: ["#7B8FA1", "#A9A9A9", "#D3D3D3"],
+    rainy: ["#001F3F", "#0077B6", "#00B4D8"],
+    thunderstorm: ["#2C003E", "#6600CC", "#FF00FF"],
   };
 
   useEffect(() => {
@@ -40,66 +43,91 @@ function Weather() {
   }, []);
 
   async function fetchWeather() {
-    const res = await fetch(
-      "https://hagonoytides-backend-1.onrender.com/weather?city=Hagonoy"
+    try {
+      const res = await fetch(
+        "https://hagonoytides-backend-1.onrender.com/weather?city=Hagonoy"
+      );
+
+      const data = await res.json();
+      console.log("fetched data::::", data);
+
+      console.log("setWeather", data.weather[0]);
+      setWeather(data.weather[0]);
+
+      console.log("setLocation", data.name);
+      setLocation(data.name);
+
+      console.log("setTemperature", data.main);
+      setTemperature(data.main);
+
+      console.log("setWind", data.wind);
+      setWind(data.wind);
+
+      setFetchingData(false);
+    } catch (error) {
+      console.log(error);
+      setFetchingData(false);
+    }
+  }
+
+  if (fetchingData) {
+    return (
+      <div className="relative flex flex-row justify-between p-1 gap-5 mt-23 mx-auto rounded-4xl border border-yellow-500/30 backdrop-blur-2xl w-[95%] h-50 overflow-hidden">
+        <p>Loadingggggggggggggggggggggg......</p>
+      </div>
     );
-
-    const data = await res.json();
-    console.log("fetched data::::", data);
-
-    console.log("setWeather", data.weather[0]);
-    setWeather(data.weather[0]);
-
-    console.log("setLocation", data.name);
-    setLocation(data.name);
-
-    console.log("setTemperature", data.main);
-    setTemperature(data.main);
-
-    console.log("setWind", data.wind);
-    setWind(data.wind);
   }
 
   return (
-    <div className="relative flex flex-row justify-between p-1 gap-5 mt-23 mx-auto rounded-4xl border border-amber-400/20 backdrop-blur-2xl w-[95%] h-50 overflow-hidden">
-      <AuroraBackground
-        colorStops={weatherBasedColors.thunderstorm}
-        blend={1}
-        amplitude={1.2}
-        speed={1}
-      />
-      <div className="z-10 flex flex-row w-full backdrop-blur-3xl rounded-3xl text-white/50 px-5 py-5 md:px-20">
-        {/* Left Side */}
-        <div className="flex flex-col w-1/2 justify-between text-left">
-          <div className="">
-            <p className="text-white text-xl">
-              {time.toLocaleString("en-PH", {
-                month: "long",
-                day: "numeric",
-                year: "numeric",
-              })}
-            </p>
-            <p>{location}</p>
-          </div>
+    <div className="relative flex flex-row justify-between p-1 gap-5 mt-23 mx-auto rounded-4xl border border-yellow-500/30 backdrop-blur-2xl w-[95%] h-50 overflow-hidden">
+      {!fetchingData && location && weather && temperature && wind && (
+        <>
+          <AuroraBackground
+            colorStops={weatherBasedColors.cloudy}
+            blend={1}
+            amplitude={1.2}
+            speed={1}
+          />
+          <div className="z-10 flex flex-row w-full backdrop-blur-3xl rounded-3xl text-white/50 px-5 py-5 md:px-20">
+            {/* Left Side */}
+            <div className="flex flex-col w-1/2 justify-between text-left">
+              <div className="">
+                <p className="text-white text-lg md:text-xl">
+                  {time.toLocaleString("en-PH", {
+                    month: "long",
+                    day: "numeric",
+                    year: "numeric",
+                  })}
+                </p>
+                <p>{location}, Bulacan</p>
+              </div>
 
-          <div>
-            <p className="text-white text-5xl font-semibold">39°C</p>
-            <p>Sunny</p>
-          </div>
-        </div>
+              <div>
+                <p className="bg-linear-to-t from-white to-white/50 bg-clip-text text-transparent w-fit text-5xl font-semibold">
+                  {temperature.temp.toFixed(0)}°C
+                </p>
 
-        {/* Right Side */}
-        <div className="flex flex-col w-1/2 justify-between items-end text-right">
-          <div>
-            <p>Today</p>
-            <p>{time.toLocaleTimeString()}</p>
-          </div>
+                <p>
+                  {weather.description.charAt(0).toUpperCase() +
+                    weather.description.slice(1)}
+                </p>
+              </div>
+            </div>
 
-          <div>
-            <img src={sunnyPNG} alt="" className="w-28" />
+            {/* Right Side */}
+            <div className="flex flex-col w-1/2 justify-between items-end text-right">
+              <div>
+                <p>Today</p>
+                <p>{time.toLocaleTimeString()}</p>
+              </div>
+
+              <div>
+                <img src={cloudyPNG} alt="" className="w-28" />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 }
