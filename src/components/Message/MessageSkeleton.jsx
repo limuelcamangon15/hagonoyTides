45179c93e56234bfc0915a627a1437fa9952af23
Limuel <limuelmartincamangon@gmail.com>
2025-12-28 19:02:12 +0800

    fixed skeleton loader glitch

--- src/components/Message/MessageSkeleton.jsx
+++ src/components/Message/MessageSkeleton.jsx
@@ -4,7 +4,7 @@ function MessageSkeleton({ senderLocationWidth, messageWidth, dateWidth }) {
   return (
     <div className="flex flex-col gap-1 px-5 py-2 rounded-2xl w-full bg-gray-500/30 animate-pulse border-white/10">
       <Skeleton className={`${senderLocationWidth} h-3`} />
-      <Skeleton className={`${messageWidth} h-3`} />
+      <Skeleton className={`${messageWidth} h-5`} />
       <Skeleton className={`${dateWidth} h-3 self-end`} />
     </div>
   );
