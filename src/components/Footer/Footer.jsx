d4ca670b35ae333f9f9ce6d520515f30cc107285
Limuel <limuelmartincamangon@gmail.com>
2025-12-29 13:27:00 +0800

    adjusted footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,19 @@
-import { BiLogoGmail, BiLogoInstagram } from "react-icons/bi";
-
 function Footer() {
   return (
-    <div className="w-full h-fit p-5">
-      <p className="text-xs lg:md text-white/70 text-center">
-        &copy; {new Date().getFullYear()} HagonoyTides. All rights reserved.
-      </p>
+    <>
+      <div className="flex flex-col md:flex-row justify-between items-center border-t border-white/10 w-full md:w-4/5 h-fit p-5">
+        <p className="text-xs lg:md text-white/70 text-center">
+          &copy; {new Date().getFullYear()} HagonoyTides. All rights reserved.
+        </p>
 
-      <p className="text-xs lg:md text-white/70 text-center">
-        Powered By{" "}
-        <span className="bg-transparent bg-gradient-to-r from-green-800 to-green-400 bg-clip-text font-semibold">
-          Lims
-        </span>{" "}
-        {new Date().getFullYear()}
-      </p>
-    </div>
+        <p className="text-xs lg:md text-white/70 text-center">
+          Powered By{" "}
+          <span className=" text-green-700 font-semibold">
+            Lims {new Date().getFullYear()}
+          </span>
+        </p>
+      </div>
+    </>
   );
 }
 
