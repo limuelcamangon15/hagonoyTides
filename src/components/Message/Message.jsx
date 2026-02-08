2a3a98fda2472505b0cb0cd4f43bb02425efc54c
Limuel Camangon <limuelmartincamangon@gmail.com>
2026-02-08 12:28:20 +0800

    update: added user icon

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,9 +1,16 @@
+import { User2Icon } from "lucide-react";
 import "./message.css";
 
 function Message({ senderLocation, message, date }) {
   return (
     <div className="px-5 py-2 rounded-2xl w-full bg-zinc-800 border border-white/10">
-      <p className="text-white/50 text-xs">Someone from {senderLocation}</p>
+      <div className="flex gap-3 ">
+        <div className="flex items-center justify-center bg-gray-400 w-8 h-8 rounded-full">
+          <User2Icon className="text-gray-900" />
+        </div>
+        <p className="text-white/50 text-xs">Someone from {senderLocation}</p>
+      </div>
+
       <p className="text-white/80 pl-3">{message}</p>
       <p className="text-white/50 text-xs text-right">{date}</p>
     </div>
