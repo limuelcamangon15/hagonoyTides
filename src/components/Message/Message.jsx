a1d8a686a8a1b4272d2089199258984e867af2b2
Limuel Camangon <limuelmartincamangon@gmail.com>
2026-02-08 12:43:29 +0800

    update: fixed responsiveness of user icon

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -5,7 +5,7 @@ function Message({ senderLocation, message, date }) {
   return (
     <div className="px-5 py-2 rounded-2xl w-full bg-zinc-800 border border-white/10">
       <div className="flex gap-3 ">
-        <div className="flex items-center justify-center bg-gray-400 w-8 h-8 rounded-full">
+        <div className="flex items-center justify-center bg-gray-400 min-w-8 min-h-8 rounded-full">
           <User2Icon className="text-gray-900" />
         </div>
         <p className="text-white/50 text-xs">Someone from {senderLocation}</p>
