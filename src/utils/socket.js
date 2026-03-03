aa2cdd73555a3171fa1cf251dade5e8909018906
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-03-03 22:21:15 +0800

    update: changed api into render deployed api^

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 
 export const socket = io(
-  //"https://hagonoytides-backend-1.onrender.com"
-  "https://hagonoytides-backend-production.up.railway.app",
+  "https://hagonoytides-backend-1.onrender.com",
+  //"https://hagonoytides-backend-production.up.railway.app",
   {
     autoConnect: false,
     withCredentials: true,
